ff1eab4c90cb8a27231ab05b45f26f69863869f4
Alina Radziuk <alina.radziuk@routine-automation.com>
2022-07-18 09:55:27 +0200

    minor changes

--- ReactJS/js/AccountModal.js
+++ ReactJS/js/AccountModal.js
@@ -1,50 +1,33 @@
 let AccountModal = React.createClass({
-  createAccount: function () {
-    let accName = document.getElementById("accPhone").value;
-    let accPhone = document.getElementById("accName").value;
 
-    let acc = { Name: accName, Phone: accPhone };
-    Visualforce.remoting.Manager.invokeAction(
-      "AccountListController.createAccount",
-      acc,
-      this.creationComplete,
-      { escape: false }
-    );
-  },
+    createAccount: function () {
+        let accName = document.getElementById("accPhone").value;
+        let accPhone = document.getElementById("accName").value;
 
-  creationComplete: function () {
-    this.props.onAccountSave();
-  },
+        let acc = { Name: accName, Phone: accPhone };
+        Visualforce.remoting.Manager.invokeAction('AccountListController.createAccount', acc, this.creationComplete, { escape: false });
+    },
 
-  render() {
-    if (!this.props.showModal) {
-      return null;
+    creationComplete: function () {
+        this.props.onAccountSave();
+    },
+
+    render() {
+        if (!this.props.showModal) {
+            return null;
+        }
+        return (
+            <div>
+                <div>Test </div>
+                <label for="accName">Name:</label>
+                <input type="text" id="accName" name="accName" /> <br />
+
+                <label for="accPhone">Phone:</label>
+                <input type="text" id="accPhone" name="accPhone" /><br />
+                <button onClick={() => { this.createAccount() }}>Save</button>
+                <button onClick={() => { this.props.onClose() }}> Close </button>
+            </div>
+        );
     }
-    return (
-      <div>
-        <div>Test </div>
-        <label for="accName">Name:</label>
-        <input type="text" id="accName" name="accName" /> <br />
-        <label for="accPhone">Phone:</label>
-        <input type="text" id="accPhone" name="accPhone" />
-        <br />
-        <button
-          onClick={() => {
-            this.createAccount();
-          }}
-        >
-          Save
-        </button>
-        <button
-          onClick={() => {
-            this.props.onClose();
-          }}
-        >
-          {" "}
-          Close{" "}
-        </button>
-      </div>
-    );
-  }
-});
+})
 module.exports = AccountModal;
